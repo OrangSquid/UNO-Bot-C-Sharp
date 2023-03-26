db2c7485991543f72b75d60c054a6a920516a0a7
Nylik <nyliki.2003@gmail.com>
2023-03-26 00:13:23 +0000

    Add UNOMessageBuilder

---
+++ UNODiscordBot/UNOMessageBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Resources;
+
+namespace UNODiscordBot;
+
+internal class UNOMessageBuilder
+{
+
+    internal static readonly Dictionary<string, ulong> emojiIds;
+    static UNOMessageBuilder()
+    {
+        emojiIds = new();
+        
+        ResourceManager rm = new ResourceManager("UNODiscordBot.UNODiscordEmojis", typeof(Program).Assembly);
+        ResourceSet rs = rm.GetResourceSet(System.Globalization.CultureInfo.CurrentCulture, true, true);
+
+        foreach (DictionaryEntry resource in rs)
+        {
+            string key = resource.Key.ToString();
+            ulong value = Convert.ToUInt64(resource.Value);
+            emojiIds.Add(key, value);
+        }
+
+    }
+}
